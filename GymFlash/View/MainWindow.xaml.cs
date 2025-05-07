88c280f9fc57e4957ca9a796fa8bfbbbef594d7e
DarkAguilera <143284097+DarkAguilera@users.noreply.github.com>
2025-05-06 19:05:54 -0600

    Renombre de MainWindow a Login

--- GymFlash/View/MainWindow.xaml.cs
+++ GymFlash/View/MainWindow.xaml.cs
@@ -20,9 +20,9 @@ namespace GymFlash
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
             DataContext = new LoginViewModel();
